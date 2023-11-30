c5c3353d83d210f0173b945cb11c2c7d95929b12
Balazs Fentor <fentorb24@gmail.com>
2023-11-30 16:48:39 +0100

    Added elements to Calc

--- FashionCalc.java
+++ FashionCalc.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 import javafx.scene.control.*;
 import javafx.stage.StageStyle;
 import javafx.geometry.Pos;
+import javafx.geometry.Insets;
 import java.util.Random;
 import java.nio.charset.*;
 import java.io.BufferedReader;
@@ -15,8 +16,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.File;
 import java.io.FileWriter; 
-import java.awt.Insets;
-
+//import java.awt.Insets;
+import javafx.scene.control.TextFormatter;
 
 public class FashionCalc extends Application {
     
@@ -63,20 +64,7 @@ public class FashionCalc extends Application {
         window = primaryStage;
         primaryStage.setResizable(false);
         
-        
-        //primaryStage.initStyle(StageStyle.UNDECORATED);
-        
         /*
-        //Labels
-        Label LABmenu = new Label("Anonimity Tool GUI");
-        Label LABmacAddress = new Label("Mac Address Randomizer");
-        Label LABnameChanger = new Label("Name Changer");
-        Label LABcolon = new Label(":");
-        Label LABcolon1 = new Label(":");
-        Label LABcolon2 = new Label(":");
-        Label LABcolon3 = new Label(":");
-        Label LABcolon4 = new Label(":");
-        
         //Text Field Mac Address
         TextField TEXTmacAddress = new TextField();
         TEXTmacAddress.setPrefColumnCount(2);
@@ -117,19 +105,33 @@ public class FashionCalc extends Application {
         BUTTtest.setOnAction(e -> test());
         */
         
+        //Text Field ===============================================
+        TextField TEXTprice = new TextField();
+        TEXTprice.setPrefColumnCount(4);
+       
+        TextField TEXTcottonPercent = new TextField();
+        TEXTcottonPercent.setPrefColumnCount(2);
+        
+        TextField TEXTtest = new TextField();
+        TEXTtest.setPrefColumnCount(4);
+        
+        //Labels
         Label LABhome = new Label("Home");
         Label LABcalc = new Label("Calculator");
         Label LABsett = new Label("Settings");
+        
+        //Content
         Label LABwelcome = new Label("Welcome");
-        Label LABwelcome1 = new Label("Welcome");
-        Label LABwelcome2 = new Label("Welcome");
-       
-        //Buttons
+        Label LABcalcPrice = new Label("Price:  ");
+        Label LABcalcCottonPercent = new Label("Cotton %:  ");
+        Label LABcalcCottonPercentSign = new Label(" %");
+        Label LABcalcTest = new Label("Test:    ");
+        
+        //Buttons ===================================================
         
         //Button Menu
         Button BUTTtoMenu = new Button("Menu");
         BUTTtoMenu.setOnAction(e -> window.setScene(scene1));  
-        
         
         //Button Calculator
         Button BUTTtoCalc = new Button("Calculator");
@@ -166,7 +168,7 @@ public class FashionCalc extends Application {
         Button BUTTexit = new Button("Exit");
         BUTTexit.setOnAction(e -> System.exit(0)); 
         
-        //Layouts
+        //Layouts ====================================================
         VBox LAYHome = new VBox(20); 
         VBox LAYCalc = new VBox(20);
         VBox LAYSettings= new VBox(20);
@@ -187,19 +189,28 @@ public class FashionCalc extends Application {
         HBox HLAYmenu1 = new HBox(BUTTtoMenu1, BUTTtoCalc1, BUTTtoSett1);
         HBox HLAYmenu2 = new HBox(BUTTtoMenu2, BUTTtoCalc2, BUTTtoSett2);
         
+        HBox HLAYcontentPrice = new HBox(LABcalcPrice, TEXTprice);
+        HBox HLAYcontentCottonPercent = new HBox(LABcalcCottonPercent, TEXTcottonPercent, LABcalcCottonPercentSign);
+        HBox HLAYcontentTest = new HBox(LABcalcTest, TEXTtest);
+        
+        
         int WandH = 100;
         int contentHeight = 400;
         
-        //Set Size
-        HLAYmenu.setPrefWidth(100);
-        HLAYmenu.setPrefHeight(100);
+        //Set Size ====================================================
         
-        // Spacing for content
+        // Spacing for content 
+        
         LAYContentHome.setPrefHeight(contentHeight);
         LAYContentCalc.setPrefHeight(contentHeight);
+        LAYContentCalc.setPrefWidth(20);
         LAYContentSettings.setPrefHeight(contentHeight);
         
-        //Set Size 1
+        //Set Size 0 
+        HLAYmenu.setPrefWidth(WandH);
+        HLAYmenu.setPrefHeight(WandH);
+        
+        //Set Size 1 
         HLAYmenu1.setPrefWidth(WandH);
         HLAYmenu1.setPrefHeight(WandH);
         
@@ -207,12 +218,30 @@ public class FashionCalc extends Application {
         HLAYmenu2.setPrefWidth(WandH);
         HLAYmenu2.setPrefHeight(WandH);
         
-        //Horizontal Spacing
+        //Horizontal Spacing============================================
+        
         HLAYmenu.setSpacing(10);
         HLAYmenu1.setSpacing(10);
         HLAYmenu2.setSpacing(10);
         
-        //Alignment
+        //Alignment ====================================================
+        
+        //Labels
+        LAYLabelHome.setAlignment(Pos.TOP_CENTER);
+        LAYLabelCalc.setAlignment(Pos.TOP_CENTER);
+        LAYLabelSettings.setAlignment(Pos.TOP_CENTER);
+        
+        //Content
+        LAYContentHome.setAlignment(Pos.CENTER);
+        
+        LAYContentCalc.setAlignment(Pos.CENTER);
+        HLAYcontentPrice.setAlignment(Pos.CENTER_LEFT);
+        HLAYcontentCottonPercent.setAlignment(Pos.CENTER_LEFT);
+        HLAYcontentTest.setAlignment(Pos.CENTER_LEFT);
+        
+        LAYContentSettings.setAlignment(Pos.CENTER);
+        
+        //Menu
         LAYMenuHome.setAlignment(Pos.BOTTOM_CENTER);
         HLAYmenu.setAlignment(Pos.CENTER);
         LAYMenuCalc.setAlignment(Pos.BOTTOM_CENTER);
@@ -220,23 +249,15 @@ public class FashionCalc extends Application {
         LAYMenuSettings.setAlignment(Pos.BOTTOM_CENTER);
         HLAYmenu2.setAlignment(Pos.CENTER);
         
-        LAYLabelHome.setAlignment(Pos.TOP_CENTER);
-        LAYLabelCalc.setAlignment(Pos.TOP_CENTER);
-        LAYLabelSettings.setAlignment(Pos.TOP_CENTER);
-        
-        LAYContentHome.setAlignment(Pos.CENTER);
-        LAYContentCalc.setAlignment(Pos.CENTER);
-        LAYContentSettings.setAlignment(Pos.CENTER);
-        
-        //Add Buttons
+        //Add Buttons ====================================================
         
         LAYLabelHome.getChildren().addAll(LABhome);
         LAYLabelCalc.getChildren().addAll(LABcalc);
         LAYLabelSettings.getChildren().addAll(LABsett);
         
         LAYContentHome.getChildren().addAll(LABwelcome);        
-        LAYContentCalc.getChildren().addAll(LABwelcome1);
-        LAYContentSettings.getChildren().addAll(LABwelcome2);
+        LAYContentCalc.getChildren().addAll(HLAYcontentPrice, HLAYcontentCottonPercent, HLAYcontentTest);
+        LAYContentSettings.getChildren().addAll();
         
         LAYMenuHome.getChildren().addAll(HLAYmenu);
         LAYMenuCalc.getChildren().addAll(HLAYmenu1);
@@ -252,7 +273,7 @@ public class FashionCalc extends Application {
         LAYSettings.getChildren().addAll(LABsett, LABwelcome2, HLAYmenu2);        
         */
        
-        //Set Size 0
+        //Set Size 0 =================================================================
         BUTTtoCalc.setMinWidth(80);
         BUTTtoMenu.setMinWidth(80);
         BUTTtoSett.setMinWidth(80);
@@ -278,8 +299,12 @@ public class FashionCalc extends Application {
         BUTTtoCalc2.setMinHeight(80);
         BUTTtoMenu2.setMinHeight(80);
         BUTTtoSett2.setMinHeight(80);
+    
+        //Padding
         
-        //Assign Layouts to Scenes
+        LAYContentCalc.setPadding(new Insets(50, 50, 50, 50));
+        
+        //Assign Layouts to Scenes ====================================================
         scene1 = new Scene(LAYHome, windowX, windowY);
         scene2 = new Scene(LAYCalc, windowX, windowY);
         scene3 = new Scene(LAYSettings, windowX, windowY);
